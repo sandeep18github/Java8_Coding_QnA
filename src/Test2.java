2b67864f6c59fafdbdc13dc38e67908cd63fa405
Sandeep <ssandeepkumar045@gmail.com>
2025-06-19 21:18:33 +0530

    adding new code

--- src/Test2.java
+++ src/Test2.java
@@ -1,18 +1,15 @@
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
-
 public class Test2 {
     public static void main(String[] args) {
-         String name="Sandeep Kumar";
-         //to find repeation of A
+        String name = "Sandeep Kumar";
+        //to find repeation of A
         long count = name.toLowerCase().chars().filter(c -> c == 'a').count();
-        System.out.println(" A  -> "+ count);
+        System.out.println(" A  -> " + count);
 
         System.out.println("-----------------------------------------------------------------------");
 
@@ -21,21 +18,21 @@ public class Test2 {
                 //.replaceAll("\\s+", "")
                 .chars().mapToObj(c -> (char) c).filter(Character::isLetter)
                 //.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-                .collect(Collectors.toMap(Function.identity(), c->1L,Long::sum, LinkedHashMap::new));
+                .collect(Collectors.toMap(Function.identity(), c -> 1L, Long::sum, LinkedHashMap::new));
 
-        collect.forEach((character, c)-> System.out.println(character +" -->"+ c));
+        collect.forEach((character, c) -> System.out.println(character + " -->" + c));
 
         System.out.println("-----------------------------------------------------------------------");
 
 //below string count only letter repeation
-        String mail ="Ssandeep kumar045@gmail.com";
+        String mail = "Ssandeep kumar045@gmail.com";
 
         LinkedHashMap<Character, Long> collect1 = mail.toLowerCase().chars()
                 .mapToObj(c -> (char) c)
-               // .filter(Character::isLetter)
-                .filter(c->c =='a')
+                // .filter(Character::isLetter)
+                .filter(c -> c == 'a')
                 .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()));
-        collect1.forEach((ch,c)-> System.out.println(ch+" -> "+c+"  Original String -> "+mail));
+        collect1.forEach((ch, c) -> System.out.println(ch + " -> " + c + "  Original String -> " + mail));
 
         System.out.println("-----------------------------------------------------------------");
 
@@ -59,5 +56,18 @@ public class Test2 {
         System.out.println(count2);
 
 
+        String listOfstring = "she is is very very beautiful";
+
+        Map<String, Long> collect2 = Arrays.stream(listOfstring.toLowerCase().split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(collect2);
+
+        Long max = Collections.max(collect2.values());
+        List<String> collect3 = collect2.entrySet().stream().filter(i -> i.getValue() == max).map(Map.Entry::getKey).collect(Collectors.toList());
+        System.out.printf("Most Frequent Element:%s%nCount:%d%n",collect3,max);
+
+
+        /*Map.Entry<String, Long> stringLongEntry = collect2.entrySet().stream().max(Map.Entry.comparingByValue()).get();
+        System.out.println(stringLongEntry.getKey()+" "+stringLongEntry.getValue());
+*/
     }
 }
